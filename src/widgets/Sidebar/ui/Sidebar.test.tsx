28945618dcc8f4fdd9eb93ffa8fdea013f01493d
Michael Nakhimson <michael.nakhimson@gmail.com>
2023-09-20 21:22:51 +0300

    fixed tests

--- src/widgets/Sidebar/ui/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar.test.tsx
@@ -1,15 +1,15 @@
 import { fireEvent, screen } from '@testing-library/react'
-import { renderWithTranslation } from 'shared/lib/tests/renderWithTranslation/renderWithTranslation'
+import { componentRender } from 'shared/lib/tests/componentRender/componenRender'
 import { Sidebar } from './Sidebar'
 
 describe('Sidebar check', () => {
 	test('should render Sidebar', () => {
-		renderWithTranslation(<Sidebar />)
+		componentRender(<Sidebar />)
 		expect(screen.getByTestId('sidebar')).toBeInTheDocument()
 	})
 
 	test('should toggle', () => {
-		renderWithTranslation(<Sidebar />)
+		componentRender(<Sidebar />)
 		const toggleBtn = screen.getByTestId('sidebar-toggle')
 		expect(screen.getByTestId('sidebar')).toBeInTheDocument()
 		fireEvent.click(toggleBtn)
