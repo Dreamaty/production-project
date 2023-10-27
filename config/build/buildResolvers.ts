75edb6eb561a11d0f9d26df3a33fecebc0bec79a
Michael Nakhimson <michael.nakhimson@gmail.com>
2023-10-27 13:18:48 +0300

    little change in webpack

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -8,6 +8,8 @@ export function buildResolvers(options: BuildOptions): ResolveOptions {
 		preferAbsolute: true,
 		modules: [options.paths.src, 'node_modules'],
 		mainFiles: ['index'],
-		alias: {}
+		alias: {
+			src: options.paths.src,
+		}
 	}
 }
