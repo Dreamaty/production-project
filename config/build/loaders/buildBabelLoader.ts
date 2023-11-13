29ccd86c1995dd60d31756273bffeea4f72437d2
Michael Nakhimson <michael.nakhimson@gmail.com>
2023-11-13 19:45:45 +0200

    fix problem with build and changed github actions file

--- config/build/loaders/buildBabelLoader.ts
+++ config/build/loaders/buildBabelLoader.ts
@@ -8,24 +8,30 @@ interface BuildBabelLoaderProps extends BuildOptions {
 
 export function buildBabelLoader({ isDev, isTsx }: BuildBabelLoaderProps)  {
 	const isProd = !isDev
+
 	return {
 		test: isTsx ? /\.(tsx|jsx)$/ : /\.(js|ts)$/,
 		exclude: /node_modules/,
 		use: {
+			cacheDirectory: true,
 			loader: 'babel-loader',
 			options: {
-				cacheDirectory: true,
 				presets: ['@babel/preset-env',
 					['@babel/preset-typescript', {
 						isTsx
 					}]],
 				plugins: [
-					'@babel/plugin-transform-runtime',
-					isTsx && isProd && [ 
-						babelRemovePropsPlugin, {
-							props: ['data-testid']
-						}
+					[
+						'i18next-extract',
+						{
+							locales: ['en', 'ru'],
+							keyAsDefaultValue: true,
+						},
 					],
+					'@babel/plugin-transform-runtime',
+					isTsx && isProd && [babelRemovePropsPlugin, {
+						props: ['data-testid']
+					}],
 					isDev && require.resolve('react-refresh/babel')
 				].filter(Boolean),
 			},
