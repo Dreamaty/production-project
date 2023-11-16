5d549674385ac2c0da010d45a4c8d4f63e064c87
Michael Nakhimson <michael.nakhimson@gmail.com>
2023-11-16 10:53:36 +0200

    added component test in cypress to EditableProfileCard Changed componentRender

--- src/shared/lib/tests/componentRender/componentRender.tsx
+++ src/shared/lib/tests/componentRender/componentRender.tsx
@@ -1,24 +1,63 @@
-import { render } from '@testing-library/react'
 import { StateSchema, StoreProvider } from '@/app/providers/StoreProvider'
+// eslint-disable-next-line dreamatty-path-checker-plugin/layer-imports
+import { ThemeProvider } from '@/app/providers/ThemeProvider'
+import i18nForTests from '@/shared/config/i18n/i18nForTests'
+import { Theme } from '@/shared/const/theme'
+import { ReducersMapObject } from '@reduxjs/toolkit'
+import { render } from '@testing-library/react'
 import { ReactNode } from 'react'
 import { I18nextProvider } from 'react-i18next'
 import { MemoryRouter } from 'react-router-dom'
-import i18nForTests from '@/shared/config/i18n/i18nForTests'
+
 
 export interface componentRenderOptions {
 	route?: string
 	initialState?: DeepPartial<StateSchema>
+	asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>
+	theme?: Theme
 }
+interface TestProviderProps {
+	children: ReactNode
+	options?: componentRenderOptions
+}
+
+export function TestProvider(props: TestProviderProps) {
+	const {
+		children,
+		options = {}
+	} = props
+
+	const {
+		asyncReducers,
+		initialState,
+		route = '/',
+		theme = Theme.GREEN
+	} = options
+
+
+	return (
+		<MemoryRouter initialEntries={[route]}>
+			<StoreProvider asyncReducers={asyncReducers} initialState={initialState}>
+				<I18nextProvider i18n={i18nForTests}>
+					<ThemeProvider initialTheme={theme}>
+						<div className={`app ${theme}`}>
+							{children}
+						</div>
+					</ThemeProvider>
+				</I18nextProvider>
+			</StoreProvider>,
+		</MemoryRouter>
+	)
+}
+
 export function componentRender(
 	component: ReactNode,
 	options: componentRenderOptions = {},
 ) {
 	const { route = '/', initialState } = options
 	return render(
-		<MemoryRouter initialEntries={[route]}>
-			<StoreProvider initialState={initialState}>
-				<I18nextProvider i18n={i18nForTests}>{component}</I18nextProvider>
-			</StoreProvider>,
-		</MemoryRouter>
+		<TestProvider options={options}>
+			{component}
+		</TestProvider>
 	)
 }
